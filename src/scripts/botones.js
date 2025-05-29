11804a7158265e56688e0863eab248b1629175b2
Antonio Herrera <herreraquinteroj@gmail.com>
2025-05-29 12:44:16 -0700

    svg

---
+++ src/scripts/botones.js
@@ -0,0 +1,7 @@
+function abrirModal1(titulo) {
+    document.getElementById("titulo-modal-esp-1").innerText = titulo;
+    document.getElementById("modal-esp-1").style.display = "block";
+  }
+  function cerrarModal1() {
+    document.getElementById("modal-esp-1").style.display = "none";
+  }
